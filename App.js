3e30f5191a79b64c0419fa18abca3cd04da1b5de
WilliamEvanson <William.Evansson@target.com>
2018-08-05 19:51:48 -0500

    update text not needed in app

--- App.js
+++ App.js
@@ -27,9 +27,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
         <CreateRequest prayerRequests={this.state.requests}
-                     handler = {this.handleSubmit}>
+                       handler = {this.handleSubmit}>
         </CreateRequest>
       </View>
     );
